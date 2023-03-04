5b9090b17ed046514aca5ba7d59e7eac1d52f379
Skanda M Shastry <99529191+skandashastry15@users.noreply.github.com>
2023-03-04 11:50:27 +0530

    Add files via upload

--- BMSCE_FoodBlog/comment.js
+++ BMSCE_FoodBlog/comment.js
@@ -1,14 +1,16 @@
-let aaa=document.getElementById("aaa");
-let aa;
-let p;
-let a=document.getElementById("a");
-let date=new Date();
-
-aaa.onclick=function(){
-    p=document.getElementById("ab").innerHTML;
-     aa=document.getElementById("aa").value;
-     if(p==undefined)
-     p="ok";
-document.getElementById("ab").innerHTML=aa+"&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;"
-+date.getDate()+" - "+date.getMonth()+" - "+date.getFullYear()+"<br><br><br>"+p;
-}; 
+const form = document.getElementById('comment-form');
+    const commentsDiv = document.getElementById('comments');
+    let date=new Date();
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+      const d=date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear();
+      const name = document.getElementById('name').value;
+      const comment = document.getElementById('comment').value;
+      const newComment = document.createElement('div');
+      newComment.innerHTML = `<p><strong><span style="font-size:30px; color:grey; font-family:Comic Sans MS">${name}</span></strong><br>
+        <strong><span style="font-size:15px;">${d}</span></strong> 
+        <br><span style="font-size:20px;">${comment}</span></p><hr>`;
+      commentsDiv.appendChild(newComment);
+      document.getElementById('comment').value=null;
+      document.getElementById('name').value=null;
+    });
